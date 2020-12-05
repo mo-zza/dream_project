a1f86b211d62994293b03760666c0c91778d3b76
Kamal <kamal.ha@alchemilab.com>
2020-12-05 23:59:39 +0900

    update error

--- dream_api/config.py
+++
@@ -1,8 +0,0 @@
-import os
-
-base_dir = os.path.abspath(os.path.dirname(__file__))
-
-class Config(object):
-    SQLALCHEMY_DB_URI = os.environ.get('DATABASE_URL') or 'sqlite://' + os.path.join(base_dir, 'app.db')
-    SECRET_KEY = os.environ.get('SECRET_KEY') or "you will never guess"
-    
